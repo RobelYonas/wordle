9a6d5d5cc351e4317bc409a4bb71ebdffc7a6067
RobelYonas <robel_yonas.tesfaye_bekele0007@stud.hkr.se>
2025-08-03 23:06:44 +0200

    Refactor game logic to improve similarity function and streamline word checking process

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
 import words
 
-word = words.get_word()
 
-guessedWord = ['_']*len(word)
+def similarity(word, guess):
 
-attempts = 10
+    result = {}
 
-while attempts > 0:
+    correct_letter = {(i, letter)
+                      for i, (letter, correct) in enumerate(zip(guess, word))
+                      if letter == correct}
+    only_letter_correct = {letter for _, letter in correct_letter}
+    misplaced_letter = (set(guess) & set(word)) - only_letter_correct
+    mistake = set(word) - set(guess)
 
-    print("\nCurrent word "+"".join(guessedWord))
+    result["correct"] = only_letter_correct
+    result["misplaced"] = misplaced_letter
+    result["wrong"] = mistake
 
-    while True:
-        guess = input("Your guess; ").lower()
-        if words.check_word(guess):
+    return result, correct_letter
+
+
+def game_over(word):
+    print(f"The word was {word}")
+
+
+def main():
+
+    valid_guess = 5
+    word = words.get_word()
+    guess_words = ['_' * len(word)]*5
+
+    while valid_guess > 0:
+        guess = words.check_word()
+
+        for i in range(6):
+            guess_words[i] = guess.upper()
+            result, correct_word = similarity(guess, word)
+
+        for index, letter in correct_word:
+            guess_words[index] = letter
+        print(guess_words)
+        print(f"Misplaced: {result['misplaced']}, Wrong: {result['wrong']}")
+
+        if guess == word:
+            print("Correct answer")
             break
-        else:
-            print("Enter a real word")
 
-    list_word = set(guess)
+    game_over(word)
 
-    if any(letter in word for letter in list_word):
-        for letter in list_word:
-            if letter in word:
-                indices = []
-                for index, value in enumerate(word):
-                    if value == letter:
-                        guessedWord[index] = letter
-        print('Good guess')
-        attempts -= 1
-        print("Attempts left: " + attempts)
 
-    if '_' not in guessedWord:
-        print("\nCongratulations!! You guessed the word: " + word)
-        break
-
-    if attempts == 0 and '_' in guessedWord:
-        print('Sorry you failed to guess the word:' + word)
-        break
+if __name__ == "__main__":
+    main()
--- words.py
+++ words.py
@@ -2,17 +2,15 @@ import requests
 
 
 def get_word():
-    url = "https://random-word-api.herokuapp.com/word"
+    url = "https://random-word-api.herokuapp.com/word?length=5"
 
     response = requests.get(url)
     return "".join(response.json())
 
 
-def check_word(word):
-    url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
-
-    response = requests.get(url)
-
-    result = True if response.status_code == 200 else False
-
-    return result
+def check_word():
+    while True:
+        word = input("Enter the word: ").lower()
+        if requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}").status_code == 200:
+            return word
+        print("Invalid word")
